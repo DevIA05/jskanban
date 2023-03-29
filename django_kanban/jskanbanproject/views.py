af88be2f41bc129485d5f2b83e9e608e35b14612
DevIA05 <dia05@polenumerique.re>
2023-03-29 23:21:56 +0400

    ajout: getDataFromDB(), récupère les données de la bd entriant en fonction de la position

--- django_kanban/jskanbanproject/views.py
+++ django_kanban/jskanbanproject/views.py
@@ -1,6 +1,41 @@
 from django.shortcuts import render
+from jskanbanproject.models import Task, Column
+import pdb; #pdb.set_trace()
 
 def index(request):
-    return render(request, "index.html")
+    data = getDataFromDB()
+    return render(request, "index.html", context = {'data': data})
+
+def getDataFromDB():
+    
+    # retrieve all columns
+    columns = Column.objects.all().order_by('position')
+
+    # create a list to store the results
+    result_list = []
+
+    # loop through the columns and retrieve their tasks
+    for column in columns:
+        tasks = Task.objects.filter(idcol=column).order_by('position')
+
+        # create a dictionary to store the column and its tasks
+        column_dict = {}
+        column_dict['id'] = str(column.id)
+        column_dict['title'] = column.title
+        column_dict['item'] = []
+
+        # loop through the tasks and add them to the dictionary
+        for task in tasks:
+            task_dict = {}
+            task_dict['id'] = task.id
+            task_dict['title'] = task.title
+            task_dict['position'] = task.position
+            column_dict['item'].append(task_dict)
+
+        # add the column dictionary to the result list
+        result_list.append(column_dict)
+
+    # print the result list
+    return result_list
 
 
