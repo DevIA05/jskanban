514d28a33375725b5d9b25777f69a089d659c8af
Idriss K <dia05@polenumerique.re>
2023-03-29 09:58:00 +0400

    ajout: lors d'une création d'une nouvelle tâche l'id est incrémenté + la fonction listener submit s'applique au forme dans lequel se trouve le bouton

--- demo_riktar_jkanban/dist/customfunctions.js
+++ demo_riktar_jkanban/dist/customfunctions.js
@@ -11,26 +11,36 @@ function modifyTask(el){
     const idItem = el.getAttribute("data-eid")
     const boardId = KanbanTest.getParentBoardID(idItem)
 
+    console.warn(idItem)
+
     // Create form to edit the text
-    var formItem = document.createElement("form");
+    const formItem = document.createElement("form");
     formItem.setAttribute("class", "itemform");
     formItem.innerHTML = `<div class="form-group"><textarea class="form-control" rows="2" autofocus>${title}</textarea></div><div class="form-group"><button type="submit" class="btn btn-primary btn-xs pull-right">Edit</button><button type="button" id="CancelBtn" class="btn btn-default btn-xs pull-right">Cancel</button></div>`;
     KanbanTest.addForm(boardId, formItem);
     
+    
     // Click on Change button
     formItem.addEventListener("submit", function(e) {
         e.preventDefault();
-        var text = e.target[0].value;         // get the text in the textarea
+        const form = e.target.closest("form"); // Récupérer l'élément form parent
+        const textarea = form.querySelector("textarea"); // Récupérer le textarea à l'intérieur du form
+        const text = textarea.value; // Récupérer la valeur du textarea
+        console.log(text); // Afficher la valeur dans la console
         KanbanTest.replaceElement(idItem, {   // replace the item with new info
-            id: text,
             title: text
         })
-        formItem.parentNode.removeChild(formItem);  // remove form from html structure after modification
+        //formItem.parentNode.removeChild(formItem);  // remove form from html structure after modification
+        form.remove();
     });
    
     // Remove the form when Cancel button is clicked
     document.getElementById("CancelBtn").onclick = function() {
-        formItem.parentNode.removeChild(formItem);
+        // formItem.parentNode.removeChild(formItem);
+        const form = this.closest('form');
+        console.log(form.querySelector("textarea"))
+        // Supprimer le formulaire
+        form.remove();
     };
 }
 
@@ -42,7 +52,27 @@ function disableAction(){
     
 }
 
-
+/** SVG delete icon
+ * 
+ * @returns {string}, svg tag
+ */
 function deleteIcon(){
     return '<svg width="24" height="24" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M9.87871 14.1213L12 12M14.1213 9.87868L12 12M12 12L9.87871 9.87868M12 12L14.1213 14.1213" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/> <path d="M21 3.6V20.4C21 20.7314 20.7314 21 20.4 21H3.6C3.26863 21 3 20.7314 3 20.4V3.6C3 3.26863 3.26863 3 3.6 3H20.4C20.7314 3 21 3.26863 21 3.6Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/> </svg>'
+}
+
+/** Create the id for a new task
+ * in order to have a unique id (by adding 1) for a given column
+ * @param {int} idBoard 
+ * @returns {int} the new unique id
+ */
+function getLastIdItemOfBoard(idBoard){
+    const tasks = KanbanTest.getBoardElements(idBoard)
+    let maxEid = 0;
+    tasks.forEach(task => {
+        const eid = task.getAttribute('data-eid');
+        if (!isNaN(eid)) {
+            maxEid = Math.max(maxEid, Number(eid));
+        }   
+    });
+    return(maxEid+1)
 }
