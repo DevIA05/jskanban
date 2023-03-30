dccacd1094206d426acbb18060a1a277bbc4297b
DevIA05 <111973830+DevIA05@users.noreply.github.com>
2023-03-30 14:25:25 +0400

    ajout: addTask
    ajoute une nouvelle tâche dans la bdd

--- django_kanban/jskanbanproject/update.py
+++ django_kanban/jskanbanproject/update.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from jskanbanproject.models import Task, Column
 import pdb; #pdb.set_trace()
+from django.db.models import Max
 
 #** Update boards position
 def boardPosition(request):
@@ -27,7 +28,6 @@ def boardPosition(request):
             column.save()
         draggedCol.position = newPos; draggedCol.save()     
 
-                     
     return JsonResponse({"response": "success"})
 
 #** Edit tasks
@@ -38,6 +38,7 @@ def editTask(request):
         Task.objects.filter(id=id).update(title=title)
     return JsonResponse({"response": "success"})
 
+#** Update tasks position
 def tasksPosition(request):
     if request.method == "POST":
         idBoard = int(request.POST.get("data[idBoard]"))
@@ -45,5 +46,22 @@ def tasksPosition(request):
         
         for id in range(0, len(idTasks)):
             Task.objects.filter(id=idTasks[id]
-                                ).update(position=id+1, 
+                                ).update(position=id+1,  # la base de donnée a été rempli avec une position 
+                                                         # commençant à partir de 1, dans jskanban l'index 
+                                                         # commence à partir de 0.
                                          idcol=Column.objects.get(id=idBoard))
+    return JsonResponse({"response": "success"})
+
+def addTask(request):
+    if request.method == "POST":
+        title = request.POST.get("data[title]")
+        idBoard = request.POST.get("data[boardId]")
+        max_position = Task.objects.filter(idcol_id=idBoard).aggregate(Max('position'))['position__max']
+        if(max_position is None): max_position = 1
+        Task(title=title, idcol=Column.objects.get(id=idBoard), position=max_position).save()
+        idTask = Task.objects.get(title=title).id
+        return JsonResponse({"idTask": idTask, "code": "addTask"})
+
+
+    
+
