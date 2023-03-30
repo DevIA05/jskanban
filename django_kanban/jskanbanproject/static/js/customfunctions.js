f3ceabe3908009f6faef27ef7f27ae1277efc8c8
DevIA05 <111973830+DevIA05@users.noreply.github.com>
2023-03-30 15:58:29 +0400

    en cours: fonction modifyTitle, met à jour le titre d'une colonne

--- django_kanban/jskanbanproject/static/js/customfunctions.js
+++ django_kanban/jskanbanproject/static/js/customfunctions.js
@@ -41,14 +41,6 @@ function cancelForm(btnCancel){
     form.remove();
 }
 
-/** Disable creation and edit 
- * When the user clicks on an item, we deactivate the "+ add new card" button and the fact of 
- * being able to click on the item again.
- */
-function disableAction(){
-    
-}
-
 /** SVG delete icon
  * 
  * @returns {string}, svg tag
@@ -57,19 +49,26 @@ function deleteIcon(){
     return '<svg width="24" height="24" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M9.87871 14.1213L12 12M14.1213 9.87868L12 12M12 12L9.87871 9.87868M12 12L14.1213 14.1213" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/> <path d="M21 3.6V20.4C21 20.7314 20.7314 21 20.4 21H3.6C3.26863 21 3 20.7314 3 20.4V3.6C3 3.26863 3.26863 3 3.6 3H20.4C20.7314 3 21 3.26863 21 3.6Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/> </svg>'
 }
 
-/** Create the id for a new task
- * in order to have a unique id (by adding 1) for a given column
- * @param {int} idBoard 
- * @returns {int} the new unique id
- */
-function getLastIdItemOfBoard(idBoard){
-    const tasks = KanbanTest.getBoardElements(idBoard)
-    let maxEid = 0;
-    tasks.forEach(task => {
-        const eid = task.getAttribute('data-eid');
-        if (!isNaN(eid)) {
-            maxEid = Math.max(maxEid, Number(eid));
-        }   
+function modifyTitles() {
+    console.log("================== Modify titles ======================")
+    // Récupère tous les éléments avec la classe "kanban-title-board"
+    let titleElements = document.querySelectorAll('.kanban-title-board');
+
+    // Parcourt chaque élément et ajoute un écouteur d'événement pour la modification du texte
+    titleElements.forEach(function(titleElement) {
+        titleElement.contentEditable = true;
+        titleElement.addEventListener('input', function() {
+        // Vérifie si l'élément a le focus
+            if (document.activeElement !== titleElement) {
+                // Récupère le nouveau texte et l'affiche dans la console
+                let modifiedText = titleElement.textContent;
+                console.log(modifiedText);
+            }
+        });
     });
-    return(maxEid+1)
-}
+  }
+
+// ====================================================
+// ================ FUNCTION ==========================
+// ==================================================== 
+modifyTitles()
