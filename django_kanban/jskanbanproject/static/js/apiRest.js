d3730a9b8e5f0030c232a9a83923dc8f1f4aea67
Idriss K <dia05@polenumerique.re>
2023-03-30 11:28:17 +0400

    ajout: sendEditTask et extractTaskPositionInformations

--- django_kanban/jskanbanproject/static/js/apiRest.js
+++ django_kanban/jskanbanproject/static/js/apiRest.js
@@ -1,7 +1,7 @@
 /** Update in databse the position
  * depending on the element if it is an board or a task, 
  * we will apply the corresponding method
- * @param {Object} element,  
+ * @param {Object board} element,  
  */
 function sendBoardPosition(element){
     const order = element.getAttribute('data-order');
@@ -11,6 +11,41 @@ function sendBoardPosition(element){
                 type = "POST")
 }
 
+/** Get and regroup data in order to edit task
+ * 
+ * @param {int} id 
+ * @param {string} title 
+ */
+function sendEditTask(id, title){
+    dataRequest(
+        data = {"id": id,  "title": title},
+        url = "api/task",
+        type = "POST")
+}
+
+/** Get information of new tasks positions
+ * 
+ * @param {ObjectTask} el 
+ * @param {ObjectBoard} target 
+ */
+function extractTaskPositionInformations(el){
+       
+    idTask  = el.getAttribute('data-eid')            // On récupère l'id de la tâche    
+    idBoard = KanbanTest.getParentBoardID(idTask)    //   qui permettra de récupèrer l'id du board       
+    nodeList = KanbanTest.getBoardElements(idBoard)  //   et ainsi récupèrer la liste des tâches indexes selon leur positionnement (dans le board)        
+    
+    const dataEids = [];
+    nodeList.forEach(item => {
+        const dataEid = item.getAttribute('data-eid');
+        dataEids.push(dataEid);
+    });
+  
+    dataRequest(
+        data = { "idBoard": idBoard, "idTasks": dataEids},
+        url = "api/tasks",
+        type = "POST"
+    )    
+}
 
 function dataRequest(data, url, type){
     const csrf  = $('input[name="csrfmiddlewaretoken"]').val()   // collect token
