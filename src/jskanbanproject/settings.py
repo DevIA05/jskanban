ed7ec2792c4784b478eb82ecc483bc39d4c340e8
DevIA05 <dia05@polenumerique.re>
2023-04-02 18:05:21 +0400

    save

--- src/jskanbanproject/settings.py
+++ src/jskanbanproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-2pqcre5yld^tr%=@%v&b1l&!jc6txw*3mo*h-6=hf0)d+%u1hg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
